dc4899f185a2363d13d27d5819f15c157b42463f
Wyatt Evans <wae7@nau.edu>
2019-09-17 12:08:28 -0700

    Commit #3: Implemented flower-eating.

--- game.js
+++ game.js
@@ -11,7 +11,21 @@ var texture = PIXI.Texture.fromImage("cat.png");
 
 
 var grassland = new PIXI.Sprite(PIXI.Texture.fromImage("grassland.png"));
-var flower = new PIXI.Sprite(PIXI.Texture.fromImage("flower1.png"));
+
+var i;
+
+var flower_texture1 = PIXI.Texture.fromImage("flower1.png");
+var flower_texture2 = PIXI.Texture.fromImage("flower2.png");
+
+//var flower_array = [];
+/*for (i = 0; i < 5; i++)
+{
+    var flower = new PIXI.Sprite(flower_texture1);
+    flower.texture = flower_texture1;
+    flower_array[i] = flower;
+}*/
+
+// var flower = new PIXI.Sprite(PIXI.Texture.fromImage("flower1.png"));
 var tree_trunk_left = new PIXI.Sprite(PIXI.Texture.fromImage("tree1.png"));
 var tree_canopy_left = new PIXI.Sprite(PIXI.Texture.fromImage("tree2.png"));
 
@@ -37,12 +51,41 @@ map.addChild(terrain);*/
 //grassland.addChild(terrain);
 
 //terrain.addChild(flower);
-flower.anchor.x = 0.5;
-flower.anchor.y = 0.5;
-flower.position.x = 96;
-flower.position.y = 150;
-stage.addChild(flower);
+/*for (i = 0; i < flower_array.length; i++)
+{
+    flower_array[i].anchor.x = 0.5;
+    flower_array[i].anchor.y = 0.5;
+    //flower_array[i].position.x = (Math.round((Math.random(32, 960)/32)*32));
+    //flower_array[i].position.y = (Math.round((Math.random(32, 800)/32)*32));
+    flower_array[i].position.x = 96;
+    flower_array[i].position.y = 150;
+    stage.addChild(flower_array[i]);
+}*/
 
+var flower1 = new PIXI.Sprite(flower_texture1);
+flower1.texture = flower_texture1;
+
+flower1.anchor.x = 0.5;
+flower1.anchor.y = 0.5;
+flower1.position.x = Math.round(Math.floor((Math.random()*896)+64)/32)*32;
+flower1.position.y = Math.round(Math.floor((Math.random()*736)+64)/32)*32;
+//flower1.position.x = 96;
+//flower1.position.y = 160;
+stage.addChild(flower1);
+
+var flower2 = new PIXI.Sprite(flower_texture1);
+flower2.texture = flower_texture1;
+
+flower2.anchor.x = 0.5;
+flower2.anchor.y = 0.5;
+flower2.position.x = Math.round(Math.floor((Math.random()*896)+64)/32)*32;
+flower2.position.y = Math.round(Math.floor((Math.random()*736)+64)/32)*32;
+//flower1.position.x = 96;
+//flower1.position.y = 160;
+stage.addChild(flower2);
+
+
+/*
 //terrain.addChild(tree_trunk_left);
 tree_trunk_left.anchor.x = 0.5;
 tree_trunk_left.anchor.y = 0.5;
@@ -55,6 +98,8 @@ tree_canopy_left.anchor.y = 0.5;
 tree_canopy_left.position.x = 288;
 tree_canopy_left.position.y = 256;
 stage.addChild(tree_canopy_left);
+*/
+
 
 var cat = new PIXI.Sprite(texture);
 
@@ -62,7 +107,7 @@ cat.anchor.x = 0.5;
 cat.anchor.y = 0.5;
 
 cat.position.x = 512;
-cat.position.y = 416;
+cat.position.y = 448;
 
 //cat.scale.x = 0.2;
 //cat.scale.y = 0.2;
@@ -76,9 +121,10 @@ var cat_direction = 0; //Cat is facing left
 
 function keydownEventHandler(e)
 {
+  //Cat movement
   if (e.keyCode == 87) // W Key
   {
-    if (cat.position.y >= 10)
+    if (cat.position.y >= 64)
     {
       cat.position.y -= 32;
     }
@@ -86,7 +132,7 @@ function keydownEventHandler(e)
 
   if (e.keyCode == 83) // S Key
   {
-    if (cat.position.y <= 790)
+    if (cat.position.y <= 736)
     {
       cat.position.y += 32;
     }
@@ -102,7 +148,7 @@ function keydownEventHandler(e)
         cat.width = 32;
     }
 
-    if (cat.position.x >= 10)
+    if (cat.position.x >= 64)
     {
       cat.position.x -= 32;
     }
@@ -118,11 +164,45 @@ function keydownEventHandler(e)
         cat.width = 32;
     }
 
-    if (cat.position.x <= 990)
+    if (cat.position.x <= 896)
     {
       cat.position.x += 32;
     }
   }
+
+   //Interaction
+    if (e.keyCode == 69) // E Key
+    {
+        if (cat.position.x >= flower1.position.x &&
+            cat.position.x <= flower1.position.x+32 &&
+            cat.position.y >= flower1.position.y &&
+            cat.position.y <= flower1.position.y+32)
+        {
+            var flower_eaten = new PIXI.Sprite(flower_texture2);
+            flower_eaten.texture = flower_texture2;
+
+            flower_eaten.anchor.x = 0.5;
+            flower_eaten.anchor.y = 0.5;
+            flower_eaten.position.x = cat.position.x;
+            flower_eaten.position.y = cat.position.y;
+            stage.addChild(flower_eaten);
+            stage.addChild(cat);
+
+
+            flower1.position.x = Math.round(Math.floor((Math.random()*896)+64)/32)*32;
+            flower1.position.y = Math.round(Math.floor((Math.random()*736)+64)/32)*32;
+            flower2.position.x = Math.round(Math.floor((Math.random()*896)+64)/32)*32;
+            flower2.position.y = Math.round(Math.floor((Math.random()*736)+64)/32)*32;
+
+        }
+        if (cat.position.x >= flower2.position.x &&
+            cat.position.x <= flower2.position.x+32 &&
+            cat.position.y >= flower2.position.y &&
+            cat.position.y <= flower2.position.y+32)
+        {
+
+        }
+    }
 }
 
 document.addEventListener("keydown", keydownEventHandler);
